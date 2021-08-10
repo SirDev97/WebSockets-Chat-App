0a6e668fad3a09c9c943c774871a64a33ca35932
Alkin Maystorov <alkin971@gmail.com>
2021-08-10 17:08:37 +0200

    add Socket.io Setup on the Server and Frontend

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const socket = require("socket.io");
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+const server = app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT}!`)
+);
 
 // Static files
 app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Made Socket Connection", socket.id);
+});
